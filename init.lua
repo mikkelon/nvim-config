e309966caff75836bece018048b8aa1d33f6fa56
Mikkel Overgaard <mikkelovergaardnielsen@gmail.com>
2024-03-07 17:14:32 +0100

    Fixed cursor centering on <C-u> and <C-d>

--- init.lua
+++ init.lua
@@ -625,11 +625,12 @@ cmp.setup {
 
 -- [[ Configure Neoscroll ]]
 require('neoscroll').setup({
-  pre_hook = function(info) if info == 'centercursor' then vim.api.nvim_feedkeys('<S-M>', 'n', true) end end,
+  pre_hook = function(info) if info == 'centerc_pre' then vim.api.nvim_feedkeys('<S-M>', 'n', true) end end, -- Doesn't seem to work atm.
+  post_hook = function(info) if info == 'centerc_post' then vim.api.nvim_feedkeys('zz', 'n', true) end end
 })
 local t = {}
-t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '350', 'sine', [['centercursor']] } }
-t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '350', 'sine', [['centercursor']] } }
+t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '350', 'sine', [['centerc_post']] } }
+t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '350', 'sine', [['centerc_post']] } }
 
 require('neoscroll.config').set_mappings(t)
 
