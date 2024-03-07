6b14315c96ae5a55de57f2d1964675970ef35657
Mikkel Overgaard <mikkelovergaardnielsen@gmail.com>
2024-03-07 17:25:21 +0100

    Moved Nescroll out of init.lua

--- init.lua
+++ init.lua
@@ -623,17 +623,6 @@ cmp.setup {
   },
 }
 
--- [[ Configure Neoscroll ]]
-require('neoscroll').setup({
-  pre_hook = function(info) if info == 'centerc_pre' then vim.api.nvim_feedkeys('<S-M>', 'n', true) end end, -- Doesn't seem to work atm.
-  post_hook = function(info) if info == 'centerc_post' then vim.api.nvim_feedkeys('zz', 'n', true) end end
-})
-local t = {}
-t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '350', 'sine', [['centerc_post']] } }
-t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '350', 'sine', [['centerc_post']] } }
-
-require('neoscroll.config').set_mappings(t)
-
 -- [[ Configure Leap ]]
 require('leap').create_default_mappings()
 
--- lua/custom/plugins/neoscroll.lua
+++ lua/custom/plugins/neoscroll.lua
@@ -1,7 +1,17 @@
 return {
   "karb94/neoscroll.nvim",
-  config = function ()
-    require('neoscroll').setup {
-    }
+  config = function()
+    vim.defer_fn(function()                                                                                        -- Set a delay to ensure correct loading
+      require('neoscroll').setup({
+        pre_hook = function(info) if info == 'centerc_pre' then vim.api.nvim_feedkeys('<S-M>', 'n', true) end end, -- Doesn't seem to work atm.
+        post_hook = function(info) if info == 'centerc_post' then vim.api.nvim_feedkeys('zz', 'n', true) end end
+      })
+
+      local t = {}
+      t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '350', 'sine', [['centerc_post']] } }
+      t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '350', 'sine', [['centerc_post']] } }
+
+      require('neoscroll.config').set_mappings(t)
+    end, 100)
   end
 }
