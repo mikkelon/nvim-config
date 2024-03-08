55af0b04ab0eb0f0faaab371df0f97ce84eb9e60
Mikkel Overgaard <mikkelovergaardnielsen@gmail.com>
2024-03-08 10:53:15 +0100

    Switch to catppuccin theme

--- init.lua
+++ init.lua
@@ -152,21 +152,17 @@ require('lazy').setup({
       end,
     },
   },
-
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
-    lazy = false,
     config = function()
-      require('onedark').setup {
-        -- Set a style preset. 'dark' is default.
-        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-      }
-      require('onedark').load()
-    end,
+      require("catppuccin").setup({
+        flavour = "frappe"
+      })
+      vim.cmd.colorscheme "catppuccin"
+    end
   },
-
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
