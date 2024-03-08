8974f9af5478d9f9d16dd8aef543a92e5216ef27
Mikkel Overgaard <mikkelovergaardnielsen@gmail.com>
2024-03-08 10:53:00 +0100

    add tmux-navigator

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 -- [[ Kickstart keymaps ]]
 -- Keymaps for running scripts, programs, etc.
-map({'n', 'v'}, '<F9>', '<cmd>!python3 %<cr>', { desc = "Run python3 on current buffer", silent = true })
+map({ 'n', 'v' }, '<F9>', '<cmd>!python3 %<cr>', { desc = "Run python3 on current buffer", silent = true })
 
 -- See `:help vim.keymap.set()`
 map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -24,10 +24,10 @@ map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window wi
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Move to window using the <ctrl> hjkl keys
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+-- map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+-- map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+-- map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+-- map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- Center cursor when scrolling using <C-d> and <C-u>
 map("n", "<C-u>", "<C-u>zz");
@@ -36,3 +36,9 @@ map("n", "<C-d>", "<C-d>zz");
 -- [[ Keymaps for neo-tree ]]
 -- Open NeoTree with <space>e
 map('n', '<leader>e', ":Neotree reveal<cr>", { silent = true })
+
+-- [[ Keymaps for tmux-navigator ]]
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<cr>", { desc = "Window left", remap = true })
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<cr>", { desc = "Window right", remap = true })
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<cr>", { desc = "Window down", remap = true })
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<cr>", { desc = "Window up", remap = true })
